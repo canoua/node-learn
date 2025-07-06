087024223c5a8229ee24fbe6a38e39454123ec7f
akaryaka <a.karyaka.2dit@gmail.com>
2025-07-06 06:28:20 +0600

    begin learn

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-import { square, cube } from './math.js';
+// import { square, cube } from './math.js';
 
-let res  = square(2) + cube(3);
-console.log(res);
+// let res  = square(2) + cube(3);
+// console.log(res);
+import fs from 'fs';
+
+let text = fs.readFileSync('readme.txt', 'utf8');
+fs.writeFileSync('readme.txt', 'text');
+
+let obj = {
+	'file1.txt': 'text1',
+	'file2.txt': 'text2',
+	'file3.txt': 'text3',
+}
+
+for(let item in obj) {
+  fs.writeFileSync(item, obj[item]);
+}
+
+console.log(text);
+ 
